0e63f631466fdf2a007bf4832e8152911835c7be
Ann201190 <74066708+Ann201190@users.noreply.github.com>
2021-11-30 16:24:04 +0200

    master - my calc;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// использовала двухсторонюю привязку ngModel
 export class AppComponent {
-  title = 'calc';
+  title = 'Калькулятор';
+
+  objAB = { a: 0, b: 0, rezalt: 0, radio: "" }
+
+  onClickBtn() {
+    if (this.objAB.radio === "+") {
+      this.objAB.rezalt = Number(this.objAB.a) + Number(this.objAB.b);
+    }
+    else if (this.objAB.radio === "-") {
+      this.objAB.rezalt = Number(this.objAB.a) - Number(this.objAB.b);
+    }
+    else if (this.objAB.radio === "/") {
+      this.objAB.rezalt = Number(this.objAB.a) / Number(this.objAB.b);
+    }
+    else if (this.objAB.radio === "*") {
+      this.objAB.rezalt = Number(this.objAB.a) * Number(this.objAB.b);
+    }
+  }
 }
+
+
+
+
